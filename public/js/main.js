2832f17c8f8cb0c5c6f56dbe09d09a97b8df2cef
Eeemlo <emma.lorensson@outlook.com>
2024-04-17 09:56:45 +0200

    DOM-maniupulation

--- public/js/main.js
+++ public/js/main.js
@@ -4,67 +4,117 @@ async function getData() {
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data); 
+    console.log(data);
+
+    iterateData(data);
 }
 
 getData();
 
-async function createJob(companyName, jobtitle, location, startdate, enddate, description) {
+async function iterateData(data) {
+    const joblistContainer = document.querySelector(".joblist");
+    const jobExperience = data.rows;
+
+    jobExperience.forEach((job) => {
+        // Kontrollera om startdate är mer än 10 tecknen
+        const shortenedStartdate =
+            job.startdate.length > 10
+                ? job.startdate.substring(0, 10)
+                : job.startdate;
+        // Kontrollera att enddate är av typen string och innehåller mer än 10 tecknen
+        const shortenedEnddate =
+            typeof job.enddate === "string" && job.enddate.length > 10
+                ? job.enddate.substring(0, 10)
+                : job.enddate;
+
+        if (job.enddate != null) {
+            joblistContainer.innerHTML += `<div class="job"><div><h3>${job.job_title} @ </h3>
+        <h3>${job.company_name}</h3>
+        <h4>${shortenedStartdate} - ${shortenedEnddate}</h4>
+        <p>${job.description}</p>
+        <button class="editBtn">Redigera</button><button class="deleteBtn">Radera</button>
+        </div></div>
+        `;
+        } else {
+            joblistContainer.innerHTML += `<div class="job"><div><h3>${job.job_title} @ </h3>
+            <h3>${job.company_name}</h3>
+            <h4>${shortenedStartdate} - Pågående</h4>
+            <p>${job.description}</p>
+            <button class="editBtn">Redigera</button><button class="deleteBtn">Radera</button></div></div>`;
+        }
+    });
+}
+
+async function createJob(
+    companyName,
+    jobtitle,
+    location,
+    startdate,
+    enddate,
+    description
+) {
     let workExperience = {
         companyName: companyName,
         jobtitle: jobtitle,
         location: location,
         startdate: startdate,
         enddate: enddate,
-        description: description
-    }
+        description: description,
+    };
 
     const response = await fetch(url, {
         method: "POST",
         headers: {
-            "content-type": "Application/json"
+            "content-type": "Application/json",
         },
-        body: JSON.stringify(workExperience)
+        body: JSON.stringify(workExperience),
     });
 
     const data = await response.json();
     console.log(data);
-};
+}
 
-async function updateJob(id, companyName, jobtitle, location, startdate, enddate, description) {
+async function updateJob(
+    id,
+    companyName,
+    jobtitle,
+    location,
+    startdate,
+    enddate,
+    description
+) {
     let workExperience = {
         companyName: companyName,
         jobtitle: jobtitle,
         location: location,
         startdate: startdate,
         enddate: enddate,
-        description: description
+        description: description,
     };
 
-    const response = await fetch(url + '/' + id, { 
-        method: "PUT", 
+    const response = await fetch(url + "/" + id, {
+        method: "PUT",
         headers: {
-            "Content-Type": "application/json"
+            "Content-Type": "application/json",
         },
-        body: JSON.stringify(workExperience)
+        body: JSON.stringify(workExperience),
     });
 
     const data = await response.json();
     console.log(data);
-};
+}
 
 async function deleteJob(id) {
-    const response = await fetch(url + '/' + id, { 
-        method: "DELETE", 
+    const response = await fetch(url + "/" + id, {
+        method: "DELETE",
         headers: {
-            "Content-Type": "application/json"
-        }
+            "Content-Type": "application/json",
+        },
     });
 
     const data = await response.json();
     console.log(data);
-};
-
+}
 
 //Ladda in DOM innan JS körs
 document.addEventListener("DOMContentLoaded", function () {
